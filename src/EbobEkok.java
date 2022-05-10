fd129b1a987940792a388296fcf2545ad1b962aa
mehmet turkoz <mturkozz42@gmail.com>
2022-05-10 08:26:26 +0300

    ebob-ekok ödevi while ile yapıldı

---
+++ src/EbobEkok.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class EbobEkok {
+    public static void main(String[] args){
+
+        Scanner input=new Scanner(System.in);
+        int number1,number2;
+        System.out.print("1.Sayiyi Giriniz:");
+        number1= input.nextInt();
+        System.out.print("2.Sayiyi Giriniz:");
+        number2= input.nextInt();
+
+        int min = (number1 < number2) ? number1:number2;
+        int ebob=1;
+        int i=1;
+
+        while (i<=min){
+            if(number1%i==0 && number2%i==0){
+               ebob=i;
+            }
+            i++;
+        }
+        System.out.println(ebob);
+        System.out.println((number1*number2)/ebob);
+
+    }
+}
